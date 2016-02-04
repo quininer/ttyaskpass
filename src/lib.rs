635159cade6a9b14625319837c9fa7a94db5e18c
quininer kel <quininer@live.com>
2016-02-04 20:28:40 +0800

    use into

--- src/lib.rs
+++ src/lib.rs
@@ -14,28 +14,27 @@ use colorhash256::hash_as_ansi;
 
 pub fn askpass(star: &[u8]) -> SecStr {
     let star = if star.len() == 0 { "~".as_bytes() } else { star };
-    SecStr::new(
-        interactor::read_from_tty(|buf, b, tty| {
-            if b == 4 {
-                tty.write(b"\r                                \r").unwrap();
-                return;
-            };
-            let colors = match buf.len() {
-                0 => [30; 8],
-                1...7 => hash_as_ansi(&vec![random(); 8]),
-                _ => hash_as_ansi(buf)
-            };
-            let color_string = format!(
-                "\rPassword: {}",
-                ANSIStrings(
-                    &star.iter().cycle().take(8).cloned().collect::<Vec<u8>>()
-                        .chunks(2)
-                        .zip(&colors[..4])
-                        .map(|(s, &c)| Fixed(c as u8).paint(String::from_utf8_lossy(s)))
-                        .collect::<Vec<ANSIString>>()
-                )
-            ).into_bytes();
-            tty.write(&color_string).unwrap();
-        }, true, true).unwrap()
-    )
+    let star = star.iter().cycle().take(8).cloned().collect::<Vec<u8>>();
+    interactor::read_from_tty(|buf, b, tty| {
+        if b == 4 {
+            tty.write(b"\r                                \r").unwrap();
+            return;
+        };
+        let colors = match buf.len() {
+            0 => [30; 8],
+            1...7 => hash_as_ansi(&vec![random(); 8]),
+            _ => hash_as_ansi(buf)
+        };
+        let color_string = format!(
+            "\rPassword: {}",
+            ANSIStrings(
+                &star
+                    .chunks(2)
+                    .zip(&colors[..4])
+                    .map(|(s, &c)| Fixed(c as u8).paint(String::from_utf8_lossy(s)))
+                    .collect::<Vec<ANSIString>>()
+            )
+        ).into_bytes();
+        tty.write(&color_string).unwrap();
+    }, true, true).unwrap().into()
 }
